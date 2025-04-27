8fd03dd264a42110188819dad12c1a58beb13030
sadicangel <sadicangel@hotmail.com>
2025-04-27 23:19:29 +0100

    More api into ChatService folder

---
+++ src/ChatService/LetsTalk.WebApi/Program.cs
@@ -0,0 +1,57 @@
+﻿using System.Security.Claims;
+using LetsTalk.WebApi.Services;
+using Microsoft.AspNetCore.Authentication;
+using Scalar.AspNetCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.AddServiceDefaults();
+
+
+builder.Services.AddAuthentication("TestAuth")
+    .AddCookie("TestAuth");
+builder.Services.AddAuthorization();
+
+builder.Services.AddOpenApi();
+builder.Services.AddSignalR(options => options.EnableDetailedErrors = builder.Environment.IsDevelopment());
+
+var app = builder.Build();
+
+app.MapDefaultEndpoints();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    app.MapScalarApiReference();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.Use(async (context, next) =>
+{
+    if (context.User.Identity?.IsAuthenticated is not true)
+    {
+        await context.SignInAsync("TestAuth", new ClaimsPrincipal(new ClaimsIdentity([
+            new Claim(ClaimTypes.NameIdentifier, Guid.NewGuid().ToString()),
+            new Claim(ClaimTypes.Name, "User" + Random.Shared.Next(1000, 9999))
+        ], "TestAuth")));
+    }
+
+    await next(context);
+});
+
+app.MapGet("/user", (HttpContext context) => new
+{
+    Id = context.User.Claims.FirstOrDefault(x => x.Type == ClaimTypes.NameIdentifier)?.Value,
+    context.User.Identity?.Name
+});
+
+app.MapHub<ChatHub>("/chat", options =>
+{
+});
+
+app.Run();
---
+++ src/ChatService/LetsTalk.WebApi/Services/ILetsTalkClient.cs
@@ -0,0 +1,13 @@
+﻿namespace LetsTalk.WebApi.Services;
+
+public interface ILetsTalkClient
+{
+    //Task OnMessage(MessageEvent @event);
+    //Task OnNotification(NotificationEvent @event);
+
+    //Task OnUserConnected(UserConnectedEvent @event);
+    //Task OnUserDisconnected(UserDisconnectedEvent @event);
+
+    //Task OnChannelMemberJoined(ChannelMemberJoinedEvent @event);
+    //Task OnChannelMemberLeft(ChannelMemberLeftEvent @event);
+}
